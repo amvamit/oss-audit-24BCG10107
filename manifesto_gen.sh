bf841ee4b12ae4d52b050d632c96d27a8014c211
amvamit <amitiit2023@gmail.com>
2026-03-31 21:57:29 +0530

    Update .sh

--- manifesto_gen.sh
+++ manifesto_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 5: Open Source Manifesto Generator
-# Author: Nitin Yadav
+# Author: Amit Kumar Vishwakarma
 
 echo "Answer three questions to generate your manifesto."
 echo "------------------------------------------------"
